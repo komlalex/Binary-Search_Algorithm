dfbd554d755ac31e48eca7b7bc75b8f90440eed5
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 12:54:51 +0000

    Iterative binary search

--- binary_search_alg.py
+++ binary_search_alg.py
@@ -1,5 +1,16 @@
-def binary_search(list, item):
-    
+def binary_search(list, target):
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        midpoint = (first + last) // 2
+        if list[midpoint] == target:
+            return midpoint
+        elif list[midpoint] < target:
+            first = midpoint + 1
+        else:
+            last = midpoint - 1
+    return None
 
 numbers = [1,2,3,4,5,6,7,8]
 
